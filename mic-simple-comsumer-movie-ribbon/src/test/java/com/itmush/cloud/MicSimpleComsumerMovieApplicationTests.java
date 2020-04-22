90b3023c64fa8be7f899968ad9917c0ae7c401d3
dafubin <dafubin@sangon.com>
2020-04-22 16:23:14 +0800

    add ribbon

---
+++ mic-simple-comsumer-movie-ribbon/src/test/java/com/itmush/cloud/MicSimpleComsumerMovieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itmush.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicSimpleComsumerMovieApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
